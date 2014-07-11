5fbb927b7e070a262e19ab191ab8fa914193da94
Cem Onyuksel <Cem@opal.(none)>
2014-07-11 03:19:04 -0700

    keypress handling

--- util/iface.js
+++ util/iface.js
@@ -10,6 +10,7 @@ var charm = require("charm")();
 var sprintf = require("sprintf-js").sprintf;
 var Config = require("../config");
 var Demon = require("../demon");
+var keypress = require('keypress');
 
 // interactive interface
 function Iface() {
@@ -56,7 +57,8 @@ function Iface() {
     };
     
     this.print = print.bind(this);
-    this.consoleM = ["1", "2", "3"];
+    this.consoleM = [];
+    this.commandM = '';
     
     // setup interface
     charm.pipe(process.stdout);
@@ -64,17 +66,46 @@ function Iface() {
     prompt.message = "";
     prompt.delimiter = "";
     prompt.start();
+    keypress(process.stdin);
+    process.stdin.setRawMode(true);
+    process.stdin.setEncoding( 'utf8' );
+    process.stdin.resume();
+
+    // on any data into stdin
+    process.stdin.on('keypress', function (ch, key) {
+        if (key && key.name.length == 1) {
+            commandM += key.name;
+            drawScreen();
+        }
+        if (key && key.ctrl && key.name == 'c') {
+            charm.reset();
+            process.stdin.pause();
+        }
+        if (key && key.name == 'return') {
+            print('console', '> ' + commandM);
+            readCommand(commandM);
+            commandM = '';
+            drawScreen();
+        }
+        if (key && key.name == 'backspace') {
+            commandM = commandM.slice(0, -1);
+            drawScreen();
+        }
+    });
     
     // go
     drawScreen.apply(this);
-    readCommand();
 }
 
-function print(m, type) {
+function print(type, m) {
     switch (type) {
+    default:
     case 'console':
         // console messages tab
-        this.consoleM.push(m);
+        m.split('\n').forEach(function(l) {this.consoleM.push(l);});
+        while (this.consoleM.length > process.stdout.rows - 5) {
+            this.consoleM.shift();
+        }
         break;
             
     case 'trade':
@@ -92,6 +123,7 @@ function print(m, type) {
 
 // redraw the screen
 function drawScreen() {
+    charm.reset();
     // draw status area
     charm.position(0,0);
     charm.background('yellow');
@@ -99,40 +131,39 @@ function drawScreen() {
     charm.write("status\n");
     charm.write("status\n");
     charm.write("status\n");
+    
     // draw tab
-    charm.position(0, 5);
+    charm.position(0, process.stdout.rows - consoleM.length);
     charm.background('black');
     charm.foreground('white');
+    
     this.consoleM.forEach(function(l) { charm.write(l + '\n');});
     
     // draw command area
+    charm.foreground('green');
     charm.position(0, process.stdout.rows);
+    charm.write('> ');
+    charm.foreground('white');
+    charm.write(commandM);
 }
 
 // read and execute user command
-function readCommand() {
-    prompt.get([{properties : {command : {description : ">".green}}}],
-        function(err, result) {
-            if (err) {
-                console.log("input error!");
-                return;
-            } else {
-                var cl = result.command.split(' ');
-                var cmd = cl[0];
-                if (this.commandList[cmd]) {
-                    var parsedOpts = nopt(this.commandList[cmd].commandOpts,this.commandList[cmd].commandShort,cl,1);
-                    this.commandList[cmd].commandFunc.call(this, parsedOpts, readCommand);
-                } else {
-                    if (result.command) {
-                        console.log("unknown command " + cmd);
-                        help({}, readCommand);
-                    } else {
-                        readCommand();
-                    }
-                }
-            }
-        });
-};
+function readCommand(command) {
+    var cl = command.split(' ');
+    var cmd = cl[0];
+    if (this.commandList[cmd]) {
+        var parsedOpts = nopt(this.commandList[cmd].commandOpts,this.commandList[cmd].commandShort,cl,1);
+        this.commandList[cmd].commandFunc.call(this, parsedOpts, function() {});
+    } else {
+        if (command) {
+            this.print('console', "unknown command " + cmd);
+            help({}, function() {});
+        } else {
+            drawScreen();
+        }
+    }
+}
+
 
 // Print version
 function version(cb) {
@@ -151,10 +182,12 @@ function quit() {
 }
 
 function help(opts, cb) {
-    console.log("Available commands:");
+    var out = '';
+    out += 'Available commands:';
     for (var c in this.commandList) {
-        console.log(sprintf("  %-10s\t%s", c, this.commandList[c].commandDesc));
+        out += sprintf("\n  %-10s\t%s", c, this.commandList[c].commandDesc);
     }
+    this.print('console', out);
     cb();
 }
 
